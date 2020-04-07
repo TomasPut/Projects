b8754d960459563ee41d2c5e053e1770990b1833
Bender <tomas.putramentas@gmail.com>
2020-04-07 14:42:03 +0300

    First commit

---
+++ PP_Animals/src/main/java/Main.java
@@ -0,0 +1,35 @@
+
+public class Main {
+
+    public static void main(String[] args) {
+        // you can test how your classes work here
+    	//Cat cat = new Cat();
+    	
+    	
+    	Dog dog = new Dog();
+    	dog.bark();
+    	dog.eat();
+    	Dog fido = new Dog("Fido");
+    	fido.bark();
+    	
+    	Cat cat = new Cat();
+    	cat.purr();
+    	cat.eat();
+    	
+//    	Cat garfield = new Cat("Garfield");
+//    	garfield.purr();
+//    	
+//    	NoiseCapable dog2 = new Dog();
+//    	dog2.makeNoise();
+//    	
+//    	NoiseCapable cat2 = new Cat();
+//    	cat2.makeNoise();
+//    	
+//    	NoiseCapable cat3 = new Cat("Garfield");
+//    	cat3.makeNoise();
+//    	
+//    	Cat c = (Cat) cat;
+//    	c.purr();
+    }
+
+}
